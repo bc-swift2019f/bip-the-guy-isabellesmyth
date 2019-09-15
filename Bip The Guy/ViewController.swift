0c490e720346236d71b53bd43f4fe026f73e2193
Isabelle Smyth <smythi@bc.edu>
2019-09-15 14:52:24 -0400

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Isabelle Smyth on 9/15/19.
+//  Copyright © 2019 Isabelle Smyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
