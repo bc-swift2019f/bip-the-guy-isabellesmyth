54454949238794e3a1f284bef9e83ce2d7b752a4
Isabelle Smyth <smythi@bc.edu>
2019-09-19 11:15:20 -0400

    4.3 complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     //mark: properties
     @IBOutlet weak var imageToPunch: UIImageView!
+    var audioPlayer = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -33,10 +35,28 @@ class ViewController: UIViewController {
         self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
     }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try  audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in  \(soundName) couldn't be played as a sound")
+            }
+        }
+        else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+        
+    }
+    
     //mark: actions
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         print("hey you just touched the image")
         animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
 }
 
